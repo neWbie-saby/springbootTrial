b91963deca081b43847067dde929c62c98245e44
neWbie-saby <dsabyasachi388@gmail.com>
2023-03-21 21:16:19 -0700

    Playing with Java Objects -> JSON (via Jackson package implicity) and new Customer model created

--- src/main/java/com/ecommpackage/Main.java
+++ src/main/java/com/ecommpackage/Main.java
@@ -1,5 +1,9 @@
 package com.ecommpackage;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.PrimitiveIterator.OfDouble;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +24,49 @@ public class Main {
 
     @GetMapping("/greet")
     public GreetResponse greetJSON(){
-        return new GreetResponse("Heeeeeellllllo");
+        return new GreetResponse("Heeeeeellllllo", List.of("He", "Him", "Them"), new Person("Savita", 25, 108));
     }
 
-    record GreetResponse(String res){}
+    record Person(
+        String name, 
+        int age, 
+        double money
+    ){}
+
+    record GreetResponse(
+        String res,
+        List<String> pronouns,
+        Person person
+    ){}
+
+    //Implementation used before 'record' class for immutable objects
+    // class GreetResponse{
+    //     private final String greet;
+
+    //     GreetResponse(String g){
+    //         this.greet = g;
+    //     }
+
+    //     @Override
+    //     public String toString() {
+    //         return "GreetResponse{" + "greet='" + "\'" + "}";
+    //     }
+
+    //     public String getGreet(){
+    //         return greet;
+    //     }
+
+    //     @Override
+    //     public boolean equals(Object arg0) {
+    //         if(this == arg0) return true;
+    //         if(arg0 == null || getClass() != arg0.getClass()) return false;
+    //         GreetResponse that = (GreetResponse) arg0;
+    //         return Objects.equals(greet, that.greet);
+    //     }
+
+    //     @Override
+    //     public int hashCode() {
+    //         return Objects.hash(greet);
+    //     }
+    // }
 }
